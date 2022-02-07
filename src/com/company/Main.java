bccb8efff0f896f4fe13e847868007eadcb31e5d
Guilherme Brilhante <miudosan80@gmail.com>
2022-02-06 22:33:46 -0300

    ChessMatch, ChessPiece class, UI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,12 @@ package com.company;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board(3, 5);
-        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
